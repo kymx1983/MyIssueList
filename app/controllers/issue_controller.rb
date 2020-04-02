32539b93b0d2c9557735b5b96bec91de15878c26
kymx1983 <kymx1983@gmail.com>
2020-04-02 23:57:58 +0900

    Userコントローラの作成

--- app/controllers/issue_controller.rb
+++ app/controllers/issue_controller.rb
@@ -1,2 +1,43 @@
 class IssueController < ApplicationController
+  def index
+    @issues = Issue.all
+  end
+
+  def show
+    @issue = Issue.find_by(id:params[:id])
+  end
+
+  def edit
+    @issue = Issue.find_by(id:params[:id])
+  end
+
+  def update
+    issue = Issue.find_by(id:params[:id])
+
+    issue.title = params[:title]
+    issue.content = params[:content]
+
+    issue.save
+    redirect_to("/issue/index")
+  end
+
+  def destroy
+    @issue = Issue.find_by(id:params[:id])
+    @issue.destroy
+
+    redirect_to("/issue/index")
+  end
+
+  def new
+
+  end
+
+  def create
+    issue = Issue.new
+    issue.title = params[:title]
+    issue.content = params[:content]
+    issue.save
+
+    redirect_to("/issue/index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/login" => "user#login"
+  get "/" => "issue#index"
+
+  get "issue/index" => "issue#index"
+  get "issue/show/:id" => "issue#show"
+  get "issue/edit/:id" => "issue#edit"
+  post "issue/update/" => "issue#update"
+  get "issue/destroy/:id" => "issue#destroy"
+  get "issue/new" => "issue#new"
+  post "issue/create" => "issue#create"
+
+
+
 end
